0882ecee157743655a45b082c471baa15de39123
chetna9838 <chetnay9580@gmail.com>
2024-09-26 10:19:03 +0530

    Add files via upload

---
+++ li.py
@@ -0,0 +1,21 @@
+#List
+#Lists are used to store mulitple items in a single
+#variable
+#Lists are one of 4 built-in data types in 
+#Python used to store collections of data,
+#the other 3 are Tuple , setdictionar
+#all with different quantities and usage
+#mutable objects in python
+# mylist=["apple","banana", "cherry"]
+# print(mylist)
+# mylist.append("mango")
+# print(mylist)
+# mylist.clear 
+list=[]
+n=int(input("Enter the number of elements")) 
+for i in range (0,n):
+     lst1=int (input())
+     lst.append(lst1)
+print(lst)
+lst.sort()
+print(lst)
